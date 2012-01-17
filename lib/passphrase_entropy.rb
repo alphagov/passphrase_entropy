8c80b4061d3a3a01bed39dc6af6c45e300a8dac4
Paul Battley <pbattley@gmail.com>
2012-01-17 14:35:09 +0000

    Document.

--- lib/passphrase_entropy.rb
+++ lib/passphrase_entropy.rb
@@ -1,10 +1,21 @@
 require "zlib"
 
+# Estimate the entropy of a passphrase. This is calculated as the number of bytes
+# required to encode the passphrase on top of a Deflate stream of a preset
+# dictionary.
+#
 class PassphraseEntropy
+
+  # Instantiate a new PasswordEntropy calculator.
+  # dictionary should be a String containing a list of words; this is
+  # /usr/share/dict/words by default, which should be good for English systems.
+  #
   def initialize(dictionary=default_dictionary)
     @dictionary = dictionary
   end
 
+  # Estimate the entropy of s (in bytes)
+  #
   def entropy(s)
     zlen(s) - baseline
   end
