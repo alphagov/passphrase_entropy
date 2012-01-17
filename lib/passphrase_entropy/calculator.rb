6527662e9d3cc4dc055ec8efdd72768d1b764f7e
Paul Battley <pbattley@gmail.com>
2012-01-17 14:13:43 +0000

    Using the much faster C zlib makes prewarming unnecessary.

--- lib/passphrase_entropy/calculator.rb
+++ lib/passphrase_entropy/calculator.rb
@@ -1,4 +1,4 @@
-require "passphrase_entropy/zlib"
+require "zlib"
 require "singleton"
 
 module PassphraseEntropy
@@ -10,30 +10,13 @@ module PassphraseEntropy
     end
 
   private
-    def marshalled_file
-      File.expand_path("../data/zlib.marshal.#{RUBY_VERSION}", __FILE__)
-    end
-
     def dictionary
       File.read("/usr/share/dict/words")
     end
 
-    def warmed_deflater
-      unless File.exist?(marshalled_file)
-        z = PassphraseEntropy::Zlib::Deflate.new
-        z.deflate(dictionary)
-
-        File.open(marshalled_file, "w") do |f|
-          f << Marshal.dump(z)
-        end
-      end
-
-      Marshal.load(File.read(marshalled_file))
-    end
-
     def zlen(s)
-      z = warmed_deflater
-      out = z.deflate(s, PassphraseEntropy::Zlib::FINISH)
+      z = Zlib::Deflate.new
+      out = z.deflate(dictionary + s, Zlib::FINISH)
       z.close
       out.bytesize
     end
